b2d9489e613dc575fb8a52563015d39c6ba89e2f
Badtz Maru <badtzmaru139@gmail.com>
2016-01-25 10:37:13 -0800

    updated speak

--- speak.py
+++ speak.py
@@ -1,8 +1,11 @@
 import pyttsx
 import sys
+
 speaktext = " "
+
 for i in range(1,len(sys.argv)):
 	speaktext = speaktext + " " + sys.argv[i]
+	
 engine = pyttsx.init()
 engine.setProperty('voice', 1)
 engine.say(speaktext)
