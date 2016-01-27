a117cea785d5d259c72c772bb727a8b17ba5809e
Badtz Maru <badtzmaru139@gmail.com>
2016-01-27 09:17:00 -0800

    now uses student names

--- attendance.py
+++ attendance.py
@@ -14,7 +14,10 @@ from fuzzywuzzy import process
 def names(info,choices):
 	output = process.extractOne(info, choices)
 	return(output[0])
-	print(output[0])
+	
+def getid(name):
+	studentdict = {'Adin':55, 'Aidan':72, 'Angus':17, 'Anthony':29, 'Arturo':81, 'Asher':21, 'Aster':78, 'Audrey':31, 'Beck':85, 'Bryce':37, 'Cory':77, 'David':57, 'Easton':30, 'Eli':58, 'Elijah':20, 'Finn':42, 'Graeme':59, 'Grant':39, 'Hazel':60, 'Isaac':41, 'Bella':19, 'Jack':36, 'Jake':27, 'Jasmin':86, 'Jessie':35, 'Jon':48, 'Joshua':18, 'Joy':61, 'Justin':80, 'Kayleigh':26, 'Kyle':84, 'Madeleine':82, 'Margaux':14, 'Marissa':33, 'Milo':43, 'Miranda':76, 'Niall':32, 'Olive':51, 'Olivia':83, 'Oscar':79, 'Peyton':25, 'Quinn':7, 'River':47, 'Ruby':62, 'Sam':34, 'Sara':63, 'Sasha':64, 'Shane':65, 'Simon':40, 'Sofia':28, 'Spencer':24, 'Xin':66, 'Zane':67}
+	return(studentdict[name])
 
 fieldtriplist = ["FieldTrip","Fieldtrip","fieldtrip","field trip","field-trip"]
 offsitelist = ["Offsite","offsite","off-site","off site"]
@@ -25,31 +28,20 @@ studentid=0
 if sys.argv[1] == "help":
 	print("Incorrect Usage!")
 	print("arguments follow:")
-	print("password, statusname, returntime, info, studentid...")
+	print("password, statusname, returntime, info, student(s)...")
 	print("note that custom facilitators and offsite locations are currently unavalible")
 	print("example:")
-	print("attendance.py Integrity! Offsite 11:30 Pings 40 50 17")
+	print("attendance.py Integrity! Offsite 11:30 Pings milo easton river")
 	sys.exit()
-studentcounter = 5
+
 password = sys.argv[1]
+statusname = sys.argv[2]
+returntime = sys.argv[3]
+if len(returntime) == 4:
+	returntime = returntime[:2] + ':' + returntime[2:]
+info = sys.argv[4]
+studentid = sys.argv[5]
 
-if len(sys.argv)==4:
-	statusname = sys.argv[2]
-	studentcounter = 3
-else:
-	statusname = sys.argv[2]
-	returntime = sys.argv[3]
-	info = sys.argv[4]
-
-if len(sys.argv) > studentcounter+1:
-	studentids = []
-	for i in range(0,len(sys.argv)-studentcounter):
-		studentids.append(sys.argv[i+studentcounter])
-		print(sys.argv[i+studentcounter])
-else:
-	studentid = sys.argv[studentcounter]
-	
-print(statusname)
 chromedriver = 'C:\\Windows\\chromedriver.exe'
 browser = webdriver.Chrome(chromedriver)
 
@@ -62,15 +54,14 @@ username.send_keys(password)
 login_attempt = browser.find_element_by_name("Submit")
 login_attempt.click();
 time.sleep(3)
-if len(returntime) == 4:
-	returntime = returntime[:2] + ':' + returntime[2:]
-	print("corrected returntime")
 
 if studentid == 0:
 	for k in range(0,len(studentids)):
 		checkbox = browser.find_element_by_id(studentids[k])
 		checkbox.click()
 else:
+	studentid = getid(names(studentid,['Adin','Aidan','Angus','Anthony','Arturo','Asher','Aster','Audrey','Beck','Bryce','Cory','David','Easton','Eli','Elijah','Finn','Graeme','Grant','Hazel','Isaac','Bella','Jack','Jake','Jasmin','Jessie','Jon','Joshua','Joy','Justin','Kayleigh','Kyle','Madeleine','Margaux','Marissa','Milo','Miranda','Niall','Olive','Olivia','Oscar','Peyton','Quinn','River','Ruby','Sam','Sara','Sasha','Shane','Simon','Sofia','Spencer','Xin','Zane']))
+	studentid = str(studentid)
 	checkbox = browser.find_element_by_id(studentid)
 	checkbox.click()
 
@@ -85,17 +76,16 @@ if statusname in offsitelist:
 	gobutton = browser.find_element_by_name("offsite")
 	info = names(info, ['World Pizza', 'Uwajimaya', 'Starbucks', "Specialty's", 'Eastern Cafe', 'Oasis', 'Pings', 'Chipotle', 'Marination'])
 	browser.find_element_by_xpath("//option[@value='" + info + "']").click()
-elif statusname == "present":
-	gobutton = browser.find_element_by_name("present")
 elif statusname in checkoutlist:
 	gobutton = browser.find_element_by_name("checkout")
 elif statusname in fieldtriplist:
 	gobutton = browser.find_element_by_name("fieldtrip")
 	if info == "Krista":
 		info = "Crysta"
-		print("corrected name")
 	info = names(info,['Crysta', 'Scobie', 'Liana', 'Nic', 'Andy', 'Special_Aproved', 'Chrissy', 'Sam', 'Sieglinde', 'Anne', 'Kristin_J', 'Melinda', 'Dan', 'Judy', 'Quinn_H'])
 	browser.find_element_by_xpath("//option[@value='" + info + "']").click()
+else:
+	gobutton = browser.find_element_by_name("present")
 gobutton.click();
 time.sleep(3)
 
