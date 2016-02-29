a739090260e9f1dd1a41688fcd574c455092be7c
Badtz Maru <badtzmaru139@gmail.com>
2016-02-29 10:04:31 -0800

    worked on nltk

--- nl.py
+++ nl.py
@@ -13,10 +13,10 @@ tagged = nltk.pos_tag(tokens)
 
 for i in range(0,len(tagged)):
     #print(tagged[i])
-    if tagged[i][1] != "VBZ" and tagged[i][1] != "VBG" and tagged[i][1] != "RB" and tagged[i][1] != "DT" and tagged[i][1] != "CC" and tagged[i][1] != "MD" and tagged[i][1] != "VB" and tagged[i][1] != "IN":
+    if tagged[i][1] != "TO" and tagged[i][1] != "VBZ" and tagged[i][1] != "VBG" and tagged[i][1] != "RB" and tagged[i][1] != "DT" and tagged[i][1] != "CC" and tagged[i][1] != "MD" and tagged[i][1] != "VB" and tagged[i][1] != "IN":
         wordlist.append(tagged[i][0])
 #os.system("attendance.py " + "Bravery! " + output)
-for i in range(0,len(wordlist)-2):
+for i in range(0,len(wordlist)-3):
     if wordlist[i] == "field" and wordlist[i+1] == "trip":
         wordlist.remove(wordlist[i])
         wordlist[i] = "fieldtrip"
